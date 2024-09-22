a189b912cb3813a7704af714e5d5297e43f95383
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-09-23 01:41:34 +0530

    refactor(main): update main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import '@/assets/styles/index.css'
 import { AuthProvider } from '@/context/AuthContext'
+import { Toaster } from "@/components/ui/toaster"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider >
-    <App />
+      <App />
+      <Toaster />
     </AuthProvider>
   </StrictMode>,
 )
