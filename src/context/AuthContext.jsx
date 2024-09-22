31c8c469d41aca04f0ebc28af5a87d40aebca792
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-09-23 01:41:05 +0530

    refactor(auth): update authentication context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -36,8 +36,8 @@ export const AuthProvider = ({ children }) => {
 
   if (loading) {
     return (
-      <div className='fixed top-0 left-0 w-full h-full bg-white dark:bg-slate-900 flex items-center justify-center z-50'>
-        <p className='text-center dark:text-white flex items-center justify-center'>Loading...</p>
+      <div className='fixed top-0 left-0 w-full h-full bg-slate-900 flex items-center justify-center z-50'>
+        <p className='text-center text-white flex items-center justify-center'>Loading...</p>
       </div>
     );
   }
