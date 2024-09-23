47d62615e607a7944259056e79f54e9c969c5dc2
muhammedshamil8 <muhammedshamil008@gmail.com>
2024-09-24 02:03:28 +0530

    chore(update): delete, update, and solve issues in various components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,5 @@ export { default as Home } from './Home';
 export { default as Statitics } from './Statitics';
 export { default as Login } from './Login';
 export { default as AddData } from './AddData';
+export { default as AddGroups } from './AddGroups';
 export { default as NotFound } from './NotFound';
